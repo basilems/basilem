b297116b2bef2bdb65e7e189ce844bb699f91a36
Basteque <82060824+basilems@users.noreply.github.com>
2022-06-11 14:44:18 +0200

    Homepage changes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <nav class="bg-gradient-to-b from-orange-600 to-gray-700 flex flex-row flex-nowrap ">
-    <router-link to="/" class="flex-1 rounded-lg">Home</router-link>
-    <router-link to="/about" class="flex-1 rounded-lg">About</router-link>
-    <router-link to="/who" class="flex-1 rounded-lg">Who am I</router-link>
+  <nav class="bg-gradient-to-b from-orange-600 to-gray-700 flex items-center">
+    <div class="w-full max-w-sm justify-self-center">
+      <img src="@/assets/BM.png" class="md:h-full md:w-48 object-cover">
+    </div>
+    <div class="w-full flex">
+      <router-link to="/" class="flex-auto rounded-lg">Home</router-link>
+      <router-link to="/about" class="flex-auto rounded-lg">About</router-link>
+      <router-link to="/who" class="flex-auto rounded-lg">Who am I</router-link>
+    </div>
   </nav>
   <router-view/>
 </template>
@@ -10,7 +15,7 @@
 <style lang="scss">
 body{
   background-color: rgb(51 65 85);
-  color: rgb(203 213 225)
+  color: rgb(255, 255, 255)
 }
 
 #app {
@@ -26,7 +31,7 @@ nav {
 
   a {
     font-weight: bold;
-    color: rgb(203 213 225);
+    color: rgb(221, 227, 235);
 
     &.router-link-exact-active {
       color: #42b983;
