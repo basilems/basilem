f29c2022a35344d88cd79aa003cbe5c94b0a8252
Basteque <82060824+basilems@users.noreply.github.com>
2022-07-13 16:16:06 +0200

    new navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <nav class="bg-gradient-to-b from-orange-600 to-gray-100 dark:to-gray-700 flex items-center justify-between">
-    <div class="w-full max-w-sm self-end grow">
-      <img src="@/assets/BM.svg" class="h-full w-12 sm:w-32">
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <a href="https://flowbite.com/" class="flex items-center">
+          <img src="@/assets/BM.svg" class="h-6 mr-3 sm:h-9" alt="Website Logo">
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Basile Maillé</span>
+      </a>
+      <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        <svg class="hidden w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          <li>
+            <router-link to="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/who" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Who am I</router-link>
+          </li>
+          <li>
+            <router-link to="/about" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="hidden"></div>
-    <div class="w-full grid grid-cols-3 justify-end grow-0">
-      <router-link to="/" class="rounded-lg">Home</router-link>
-      <router-link to="/who" class="rounded-lg">Who am I</router-link>
-      <router-link to="/about" class="rounded-lg">About</router-link>
-    </div>
-
   </nav>
+
   <div class="">
     <router-view/>
   </div>
@@ -32,14 +47,6 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
 body{
   background-color: rgb(51 65 85);
   color: rgb(255, 255, 255)
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  // color: #2c3e50;
 }
 
 nav {
