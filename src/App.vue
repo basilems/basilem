bd0d7ba1e384e9d9f75ffcc9ad761adf2a9dcae6
Basteque <82060824+basilems@users.noreply.github.com>
2022-07-19 00:56:08 +0200

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav class="bg-white border-gray-200 drop-shadow-lg px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <nav class="bg-white border-gray-200 drop-shadow-lg px-2 sm:px-4 py-2.5 dark:bg-gray-900">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <a href="https://basilem.net/" class="flex items-center">
           <img src="@/assets/BM.svg" class="h-6 mr-3 sm:h-9" alt="Website Logo">
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Basile Maillé</span>
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ $route.name.charAt(0).toUpperCase() + $route.name.slice(1) }}</span>
       </a>
       <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -11,7 +11,7 @@
         <svg class="hidden w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium font-medium">
           <li>
             <router-link to="/" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Home</router-link>
           </li>
@@ -46,18 +46,5 @@ export default {
 body{
   background-color: rgb(51 65 85);
   color: rgb(255, 255, 255)
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: rgb(221, 227, 235);
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
